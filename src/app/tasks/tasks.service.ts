5b44c6766bc0985fb49c3d418b3625c8e031d8f9
Ram Kumar <ramgambit@gmail.com>
2025-06-15 17:33:57 +0530

    task service updated

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -40,6 +40,10 @@ export class TasksService {
     }
   }
 
+  getTasksByUserId(userId: string) {
+    return this.tasks().filter((task) => task.userId === userId);
+  }
+
   addTask(taskData: NewTaskData, userId: string) {
     this.tasks.update((prevTasks) => [
       {
