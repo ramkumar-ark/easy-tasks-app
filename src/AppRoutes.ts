e21cd3adccb736e02dc2e4ee89c07ee4dca3e938
Ram Kumar <ramgambit@gmail.com>
2025-06-15 15:06:28 +0530

    mapped root route

--- src/AppRoutes.ts
+++ src/AppRoutes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 import { TasksComponent } from './app/tasks/tasks.component';
+import { NoTaskComponent } from './app/tasks/no-task/no-task.component';
 
 export const AppRoutes: Routes = [
+  {
+    path: '',
+    component: NoTaskComponent,
+  },
   {
     path: 'tasks',
     component: TasksComponent,
