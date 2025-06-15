6abf2ea59e98bbd67c953a4147975ec9ef0128e5
Ram Kumar <ramgambit@gmail.com>
2025-06-15 17:57:32 +0530

    extracted dynamic route in child component

--- src/AppConfig.ts
+++ src/AppConfig.ts
@@ -1,7 +1,17 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import {
+  provideRouter,
+  withComponentInputBinding,
+  withRouterConfig,
+} from '@angular/router';
 import { AppRoutes } from './AppRoutes';
 
 export const AppConfig: ApplicationConfig = {
-  providers: [provideRouter(AppRoutes, withComponentInputBinding())],
+  providers: [
+    provideRouter(
+      AppRoutes,
+      withComponentInputBinding(),
+      withRouterConfig({ paramsInheritanceStrategy: 'always' })
+    ),
+  ],
 };
